18a4467d23ef808fd0fa6bbf952b26c6299f6bc9
JSilva723 <silvajonatanivan@gmail.com>
2024-08-18 12:33:20 -0300

    add lang controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 
 class IndexController extends AbstractController
 {
@@ -12,5 +15,24 @@ class IndexController extends AbstractController
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
+    }
+
+    public function lang(Request $request, string $lang, RouterInterface $router): RedirectResponse
+    {
+        $referer = $request->headers->get('referer');
+
+        if ($referer) {
+            $routeInfo = $router->match(parse_url($referer, PHP_URL_PATH));
+            $routeName = $routeInfo['_route'];
+            $routeParams = array_filter($routeInfo, function ($key) {
+                return strpos($key, '_') !== 0;
+            }, ARRAY_FILTER_USE_KEY);
+
+            $routeParams += ['_locale' => $lang];
+
+            return $this->redirectToRoute($routeName, $routeParams);
+        }
+
+        return $this->redirectToRoute('app_index', ['_locale' => $lang]);
     }
 }
