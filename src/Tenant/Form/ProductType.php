7ab3fdd09f6add3d5ca6bae92cfdf5549c1212b0
JSilva723 <silvajonatanivan@gmail.com>
2024-08-28 22:01:49 -0300

    run lint

--- src/Tenant/Form/ProductType.php
+++ src/Tenant/Form/ProductType.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tenant\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
