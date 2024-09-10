5cc398141e7a01a5315f8f6e1f31a755e8dda589
JSilva723 <silvajonatanivan@gmail.com>
2024-09-10 11:54:31 -0300

    refactor sale order controler, add constructor

--- src/Tenant/Controller/SaleOrderController.php
+++ src/Tenant/Controller/SaleOrderController.php
@@ -16,121 +16,116 @@ use Tenant\Form\SaleOrderCloseType;
 use Tenant\Form\SaleOrderOpenType;
 use Tenant\Repository\SaleOrderLineRepository;
 use Tenant\Repository\SaleOrderRepository;
+use Tenant\Services\ValidateParams;
 
 use function count;
-use function is_numeric;
 
 class SaleOrderController extends AbstractController
 {
-    public function index(
-        Request $request,
-        SaleOrderRepository $saleOrderRepository,
-        PaginatorInterface $paginator,
-    ): Response {
-        $status = $request->get('status', SaleOrder::STATUS_OPEN);
-        $query = $saleOrderRepository->findByStatus($status);
+    public function __construct(
+        private readonly SaleOrderRepository $saleOrderRepository,
+        private readonly ValidateParams $validateParams,
+        private readonly EntityManagerInterface $entityManager,
+    ) {}
 
-        $pagination = $paginator->paginate(
-            $query, // query NOT result
-            $request->query->getInt('page', 1), // page number
-            10, // limit per page
-        );
+    public function index(Request $request, PaginatorInterface $paginator): Response
+    {
+        $status = $request->get('status', SaleOrder::STATUS_OPEN);
+        $query = $this->saleOrderRepository->findByStatus($status);
+        $pageNeumber = $request->query->getInt('page', 1);
+        $limit = 10;
+
+        $pagination = $paginator->paginate($query, $pageNeumber, $limit);
 
         return $this->render('sale-order/index.html.twig', [
             'pagination' => $pagination,
         ]);
     }
 
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request): Response
     {
-        try {
-            $user = $entityManager->getRepository(User::class)->find($this->getUser());
+        $saleOrder = new SaleOrder();
+        /** @var User */
+        $user = $this->getUser();
+        $saleOrder->setUser($user);
 
-            if (!$user) {
-                throw new Exception('User not found');
-            }
+        $form = $this->createForm(SaleOrderOpenType::class, $saleOrder);
+        $form->handleRequest($request);
 
-            $saleOrder = new SaleOrder();
-            $saleOrder->setUser($user);
-
-            $form = $this->createForm(SaleOrderOpenType::class, $saleOrder);
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                $entityManager->persist($saleOrder);
-                $entityManager->flush();
-
-                return $this->redirectToRoute('tenant_sale_order_show', [
-                    'id' => $saleOrder->getId(),
-                    'saleOrder' => $saleOrder,
-                ], Response::HTTP_SEE_OTHER);
-            }
-
-            return $this->render('sale-order/new.html.twig', [
-                'saleOrder' => $saleOrder,
-                'form' => $form,
-            ]);
-        } catch (Exception) {
-            // TODO: handle this execption
-            return $this->redirectToRoute('tenant_sale_order_index');
-        }
-    }
-
-    public function show(
-        SaleOrder $saleOrder,
-        SaleOrderRepository $saleOrderRepository,
-        SaleOrderLineRepository $saleOrderLineRepository,
-    ): Response {
-        $form = $this->createForm(SaleOrderCloseType::class, $saleOrder, [
-            'action' => $this->generateUrl('tenant_sale_order_close', ['soid' => $saleOrder->getId()]),
-            'method' => 'POST',
-        ]);
-
-        $products = $saleOrderRepository->getProductsWhitPrice($saleOrder->getPriceList()->getId());
-        $orderLines = $saleOrderLineRepository->getLinesById($saleOrder->getId());
-
-        $total = 0;
-
-        for ($i = 0; $i < count($orderLines); $i++) {
-            $subTotal = $orderLines[$i]['quantity'] * (int) $orderLines[$i]['price'];
-            $total += $subTotal;
-            $orderLines[$i]['subTotal'] = $subTotal;
-        }
-
-        return $this->render('sale-order/show.html.twig', [
-            'saleOrder' => $saleOrder,
-            'products' => $products,
-            'orderLines' => $orderLines,
-            'form' => $form,
-            'total' => $total,
-        ]);
-    }
-
-    public function close(
-        Request $request,
-        SaleOrderRepository $saleOrderRepository,
-    ): Response {
-        try {
-            $saleOrderId = $this->getValidatedInt($request->get('soid'), 'sale order');
-            $paymentId = $this->getValidatedInt($request->get('sale_order_close')['payment'], 'payment');
-
-            $saleOrderRepository->closeOrder($saleOrderId, $paymentId);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($saleOrder);
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('tenant_sale_order_show', [
-                'id' => $saleOrderId,
+                'id' => $saleOrder->getId(),
+                'saleOrder' => $saleOrder,
             ], Response::HTTP_SEE_OTHER);
-        } catch (Exception) {
-            // TODO: handle this execption
+        }
+
+        return $this->render('sale-order/new.html.twig', [
+            'saleOrder' => $saleOrder,
+            'form' => $form,
+        ]);
+    }
+
+    public function show(Request $request, SaleOrderLineRepository $saleOrderLineRepository): Response
+    {
+        try {
+            $saleOrderId = $this->validateParams->validatedInt($request->get('id'), 'sale order');
+            $saleOrder = $this->entityManager->getRepository(SaleOrder::class)->find($saleOrderId);
+
+            if (!$saleOrder) {
+                throw new Exception('Sale order not found');
+            }
+
+            $form = $this->createForm(SaleOrderCloseType::class, $saleOrder, [
+                'action' => $this->generateUrl('tenant_sale_order_close', ['soid' => $saleOrder->getId()]),
+                'method' => 'POST',
+            ]);
+
+            $products = $this->saleOrderRepository->getProductsWhitPrice($saleOrder->getPriceList()->getId());
+            $orderLines = $saleOrderLineRepository->getLinesById($saleOrder->getId());
+
+            $total = 0;
+
+            for ($i = 0; $i < count($orderLines); $i++) {
+                $subTotal = $orderLines[$i]['quantity'] * (int) $orderLines[$i]['price'];
+                $total += $subTotal;
+                $orderLines[$i]['subTotal'] = $subTotal;
+            }
+
+            return $this->render('sale-order/show.html.twig', [
+                'saleOrder' => $saleOrder,
+                'products' => $products,
+                'orderLines' => $orderLines,
+                'form' => $form,
+                'total' => $total,
+            ]);
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
+
             return $this->redirectToRoute('tenant_sale_order_index', [], Response::HTTP_SEE_OTHER);
         }
     }
 
-    private function getValidatedInt(string $value, string $type): int
+    public function close(Request $request): Response
     {
-        if (!$value || !is_numeric($value)) {
-            throw new Exception("{$type} is required and must be a number");
-        }
+        try {
+            $saleOrderId = $this->validateParams->validatedInt($request->get('soid'), 'sale order');
+            $paymentId = $this->validateParams->validatedInt($request->get('sale_order_close')['payment'], 'payment');
 
-        return (int) $value;
+            $this->saleOrderRepository->closeOrder($saleOrderId, $paymentId);
+
+            return $this->redirectToRoute('tenant_sale_order_show', [
+                'id' => $saleOrderId,
+            ], Response::HTTP_SEE_OTHER);
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
+
+            $route = isset($saleOrderId) ? 'tenant_sale_order_show' : 'tenant_sale_order_index';
+            $params = isset($saleOrderId) ? ['id' => $saleOrderId] : [];
+
+            return $this->redirectToRoute($route, $params, Response::HTTP_SEE_OTHER);
+        }
     }
 }
