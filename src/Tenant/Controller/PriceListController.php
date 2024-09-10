07c32db15d063f20666944343bb4df1d8a3089cf
JSilva723 <silvajonatanivan@gmail.com>
2024-09-10 09:32:47 -0300

    refactor price list controller, add constructor

--- src/Tenant/Controller/PriceListController.php
+++ src/Tenant/Controller/PriceListController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tenant\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,39 +14,40 @@ use Tenant\Entity\PriceList;
 use Tenant\Form\PriceListType;
 use Tenant\Repository\PriceListRepository;
 use Tenant\Repository\ProductPriceListRepository;
+use Tenant\Services\ValidateParams;
 
 class PriceListController extends AbstractController
 {
-    public function index(
-        Request $request,
-        PriceListRepository $priceListRepository,
-        PaginatorInterface $paginator,
-    ): Response {
-        // Search by name
-        $q = $request->get('q', '');
-        $query = $priceListRepository->findByQ($q);
+    public function __construct(
+        private readonly PriceListRepository $priceListRepository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ValidateParams $validateParams,
+        private readonly PaginatorInterface $paginator,
+    ) {}
 
-        $pagination = $paginator->paginate(
-            $query, // query NOT result
-            $request->query->getInt('page', 1), // page number
-            10, // limit per page
-        );
+    public function index(Request $request): Response
+    {
+        $q = $request->get('q', '');
+        $query = $this->priceListRepository->findByQ($q);
+        $pageNeumber = $request->query->getInt('page', 1);
+        $limit = 10;
+
+        $pagination = $this->paginator->paginate($query, $pageNeumber, $limit);
 
         return $this->render('price-list/index.html.twig', [
             'pagination' => $pagination,
         ]);
     }
 
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request): Response
     {
         $priceList = new PriceList();
-
         $form = $this->createForm(PriceListType::class, $priceList);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($priceList);
-            $entityManager->flush();
+            $this->entityManager->persist($priceList);
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('tenant_price_list_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -55,47 +57,74 @@ class PriceListController extends AbstractController
         ]);
     }
 
-    public function show(
-        PriceList $priceList,
-        Request $request,
-        ProductPriceListRepository $productPriceListRepository,
-        PaginatorInterface $paginator,
-    ): Response {
-        $q = $request->get('q', '');
-        $query = $productPriceListRepository->findByQ($q, $priceList->getId());
-        $pagination = $paginator->paginate(
-            $query, // query NOT result
-            $request->query->getInt('page', 1), // page number
-            10, // limit per page
-        );
-
-        return $this->render('price-list/show.html.twig', [
-            'pagination' => $pagination,
-            'priceList' => $priceList,
-        ]);
-    }
-
-    public function edit(Request $request, PriceList $priceList, EntityManagerInterface $entityManager): Response
+    public function show(Request $request, ProductPriceListRepository $productPriceListRepository): Response
     {
-        $form = $this->createForm(PriceListType::class, $priceList);
-        $form->handleRequest($request);
+        try {
+            $priceListId = $this->validateParams->validatedInt($request->get('id'), 'price list');
+            $priceList = $this->entityManager->getRepository(PriceList::class)->find($priceListId);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            if (!$priceList) {
+                throw new Exception('Price list not found');
+            }
+
+            $q = $request->get('q', '');
+            $query = $productPriceListRepository->findByQ($q, $priceList->getId());
+            $pageNumber = $request->query->getInt('page', 1);
+            $limit = 10;
+
+            $pagination = $this->paginator->paginate($query, $pageNumber, $limit);
+
+            return $this->render('price-list/show.html.twig', [
+                'pagination' => $pagination,
+                'priceList' => $priceList,
+            ]);
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
 
             return $this->redirectToRoute('tenant_price_list_index', [], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->render('price-list/edit.html.twig', [
-            'priceList' => $priceList,
-            'form' => $form,
-        ]);
     }
 
-    public function delete(Request $request, PriceList $priceList, PriceListRepository $priceListRepository): Response
+    public function edit(Request $request): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $priceList->getId(), $request->getPayload()->getString('_token'))) {
-            $priceListRepository->disable($priceList->getId());
+        try {
+            $priceListId = $this->validateParams->validatedInt($request->get('id'), 'price list');
+            $priceList = $this->entityManager->getRepository(PriceList::class)->find($priceListId);
+
+            if (!$priceList) {
+                throw new Exception('Price list not found');
+            }
+
+            $form = $this->createForm(PriceListType::class, $priceList);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->entityManager->flush();
+
+                return $this->redirectToRoute('tenant_price_list_index', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->render('price-list/edit.html.twig', [
+                'priceList' => $priceList,
+                'form' => $form,
+            ]);
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
+
+            return $this->redirectToRoute('tenant_price_list_index', [], Response::HTTP_SEE_OTHER);
+        }
+    }
+
+    public function delete(Request $request): Response
+    {
+        try {
+            $priceListId = $this->validateParams->validatedInt($request->get('id'), 'price list');
+
+            if ($this->isCsrfTokenValid('delete' . $priceListId, $request->getPayload()->getString('_token'))) {
+                $this->priceListRepository->disable($priceListId);
+            }
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
         }
 
         return $this->redirectToRoute('tenant_price_list_index', [], Response::HTTP_SEE_OTHER);
