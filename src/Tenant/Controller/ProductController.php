55cdbcc23975f294ce7d43c5472b7c63f355b056
JSilva723 <silvajonatanivan@gmail.com>
2024-09-10 10:36:50 -0300

    refactor product controller, add constructor

--- src/Tenant/Controller/ProductController.php
+++ src/Tenant/Controller/ProductController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tenant\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,46 +15,47 @@ use Tenant\Form\ProductCreateType;
 use Tenant\Form\ProductEditType;
 use Tenant\Repository\ProductPriceListRepository;
 use Tenant\Repository\ProductRepository;
+use Tenant\Services\ValidateParams;
 
 use function base64_encode;
 use function file_get_contents;
 
 class ProductController extends AbstractController
 {
-    public function index(Request $request, ProductRepository $productRepository, PaginatorInterface $paginator): Response
+    public function __construct(
+        private readonly ProductRepository $productRepository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ValidateParams $validateParams,
+    ) {}
+
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
         $q = $request->get('q', '');
-        $query = $productRepository->findByQ($q);
+        $query = $this->productRepository->findByQ($q);
+        $pageNeumber = $request->query->getInt('page', 1);
+        $limit = 10;
 
-        $pagination = $paginator->paginate(
-            $query, // query NOT result
-            $request->query->getInt('page', 1), // page number
-            10, // limit per page
-        );
+        $pagination = $paginator->paginate($query, $pageNeumber, $limit);
 
         return $this->render('product/index.html.twig', [
             'pagination' => $pagination,
         ]);
     }
 
-    public function new(Request $request, EntityManagerInterface $entityManager, ProductPriceListRepository $productPriceListRepository): Response
+    public function new(Request $request, ProductPriceListRepository $productPriceListRepository): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductCreateType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($product);
-            $entityManager->flush();
+            $this->entityManager->persist($product);
+            $this->entityManager->flush();
 
             $priceList = $form->get('priceList')->getData();
             $price = $form->get('price')->getData();
 
-            $productPriceListRepository->create(
-                $product->getId(),
-                $priceList->getId(),
-                (float) $price,
-            );
+            $productPriceListRepository->create($product->getId(), $priceList->getId(), (float) $price);
 
             return $this->redirectToRoute('tenant_product_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -63,68 +65,111 @@ class ProductController extends AbstractController
         ]);
     }
 
-    public function show(Product $product): Response
+    public function show(Request $request): Response
     {
-        return $this->render('product/show.html.twig', ['product' => $product]);
-    }
+        try {
+            $productId = $this->validateParams->validatedInt($request->get('id'), 'product');
+            $product = $this->entityManager->getRepository(Product::class)->find($productId);
 
-    public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(ProductEditType::class, $product);
-        $form->handleRequest($request);
+            if (!$product) {
+                throw new Exception('Product list not found');
+            }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            return $this->render('product/show.html.twig', [
+                'product' => $product,
+            ]);
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
 
             return $this->redirectToRoute('tenant_product_index', [], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->render('product/edit.html.twig', [
-            'product' => $product,
-            'form' => $form,
-        ]);
     }
 
-    public function delete(Request $request, Product $product, ProductRepository $productRepository): Response
+    public function edit(Request $request): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->getPayload()->getString('_token'))) {
-            $productRepository->disable($product->getId());
+        try {
+            $productId = $this->validateParams->validatedInt($request->get('id'), 'product');
+            $product = $this->entityManager->getRepository(Product::class)->find($productId);
+
+            if (!$product) {
+                throw new Exception('Product list not found');
+            }
+
+            $form = $this->createForm(ProductEditType::class, $product);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->entityManager->flush();
+
+                return $this->redirectToRoute('tenant_product_index', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->render('product/edit.html.twig', [
+                'product' => $product,
+                'form' => $form,
+            ]);
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
+
+            return $this->redirectToRoute('tenant_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+    }
+
+    public function delete(Request $request): Response
+    {
+        try {
+            $productId = $this->validateParams->validatedInt($request->get('id'), 'product');
+
+            if ($this->isCsrfTokenValid('delete' . $productId, $request->getPayload()->getString('_token'))) {
+                $this->productRepository->disable($productId);
+            }
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
         }
 
         return $this->redirectToRoute('tenant_product_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    public function upload(Request $request, EntityManagerInterface $entityManager): Response
+    public function upload(Request $request): Response
     {
-        $file = $request->files->get('img');
+        try {
+            $file = $request->files->get('img');
 
-        if (!$file) {
-            return new Response('File not uploaded', 400);
+            if (!$file) {
+                throw new Exception('File not uploaded');
+            }
+
+            $imgData = file_get_contents($file->getPathname());
+
+            if (!$imgData) {
+                throw new Exception('File not content');
+            }
+
+            $imgBase64 = base64_encode($imgData);
+
+            $productId = $request->get('id');
+            $product = $this->entityManager->getRepository(Product::class)->find($productId);
+
+            if (!$product) {
+                throw new Exception('Product not found');
+            }
+
+            $product->setImg($imgBase64);
+            $product->setMimeType($file->getMimeType());
+
+            $this->entityManager->persist($product);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('tenant_product_show', [
+                'id' => $productId,
+            ], Response::HTTP_SEE_OTHER);
+        } catch (Exception $e) {
+            $this->addFlash('error', $e->getMessage());
+
+            $route = isset($productId) ? 'tenant_product_show' : 'tenant_product_index';
+            $params = isset($productId) ? ['id' => $productId] : [];
+
+            return $this->redirectToRoute($route, $params, Response::HTTP_SEE_OTHER);
         }
-
-        $imgData = file_get_contents($file->getPathname());
-
-        if (!$imgData) {
-            return new Response('File not content', 400);
-        }
-
-        $imgBase64 = base64_encode($imgData);
-
-        $id = $request->get('id');
-        $product = $entityManager->getRepository(Product::class)->find($id);
-
-        if (!$product) {
-            return new Response('Product not found', 400);
-        }
-
-        $product->setImg($imgBase64);
-        $product->setMimeType($file->getMimeType());
-
-        $entityManager->persist($product);
-        $entityManager->flush();
-
-        return $this->redirectToRoute('tenant_product_show', [
-            'id' => $id,
-        ], Response::HTTP_SEE_OTHER);
     }
 }
