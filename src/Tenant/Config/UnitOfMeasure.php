0e4e55d25c512c5174ee82c017e91e6b2b21b024
JSilva723 <silvajonatanivan@gmail.com>
2024-09-14 21:49:48 -0300

    refactor change units

--- src/Tenant/Config/UnitOfMeasure.php
+++ src/Tenant/Config/UnitOfMeasure.php
@@ -7,15 +7,15 @@ namespace Tenant\Config;
 enum UnitOfMeasure: int
 {
     case UNIT = 0;
-    case GRAM = 1;
-    case MILIMETERS = 2;
+    case KILOGRAM = 1;
+    case LITER = 2;
 
     public function getLabel(): string
     {
         return match ($this) {
             self::UNIT => 'Unit',
-            self::GRAM => 'Gram',
-            self::MILIMETERS => 'Milliliters',
+            self::KILOGRAM => 'Kilogram',
+            self::LITER => 'Liter',
         };
     }
 }
