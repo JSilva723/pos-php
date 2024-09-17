7dc45849bf3df6345afe2f03a60ef8b2f11314fe
JSilva723 <silvajonatanivan@gmail.com>
2024-09-17 07:21:56 -0300

    add command for create tables

--- src/Landlord/Command/RunMigrationsCommand.php
+++ src/Landlord/Command/RunMigrationsCommand.php
@@ -11,12 +11,14 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 use function sprintf;
 
-class CreateTenantDbCommand extends Command
+class RunMigrationsCommand extends Command
 {
-    protected static string $defaultName = 'landlord:create-tenant-db';
+    protected static string $defaultName = 'landlord:run-migrations';
 
     public function __construct(private readonly Connection $landlordConnection)
     {
@@ -26,25 +28,36 @@ class CreateTenantDbCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Create a new tenant database')
-            ->addArgument('tenantDbName', InputArgument::REQUIRED, 'The name of new tenant');
+            ->setDescription('Run migrations in tenant database')
+            ->addArgument('tenantDbName', InputArgument::REQUIRED, 'The name of tenant');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+
         $tenantDbName = $input->getArgument('tenantDbName');
 
-        $sql = sprintf('CREATE DATABASE IF NOT EXISTS `%s`', $tenantDbName);
+        $sql = sprintf('USE `%s`', $tenantDbName);
 
         try {
             $this->landlordConnection->executeStatement($sql);
-            $io->success(sprintf('The database "%s" has been created successfully.', $tenantDbName));
+            $io->success(sprintf('Change to database "%s".', $tenantDbName));
         } catch (Exception $e) {
-            $io->error('Error creating database: ' . $e->getMessage());
+            $io->error('Error changind database: ' . $e->getMessage());
 
             return Command::FAILURE;
         }
+
+        $io->section('Running migrations...');
+        $migrateProcess = new Process(['php', 'bin/console', 'doctrine:migrations:migrate', '--no-interaction']);
+        $migrateProcess->run();
+
+        if (!$migrateProcess->isSuccessful()) {
+            throw new ProcessFailedException($migrateProcess);
+        }
+
+        $io->success('Migrations executed successfully.');
 
         return Command::SUCCESS;
     }
